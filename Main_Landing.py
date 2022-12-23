95b1aa26bc5b56bc4e896bb66a99b3af8adbf6fc
Andrew Kroening <111026320+andrewkroening@users.noreply.github.com>
2022-12-23 13:42:10 -0500

    Minor changes to afford calc pages

--- Main_Landing.py
+++ Main_Landing.py
@@ -11,3 +11,30 @@ st.set_page_config(
 )
 
 st.title("Welcome to Fun Money Tools")
+st.write(
+    "Learning about finance is hard, and sometimes it can be really intimidating. This is a collection of tools to help you manage your money and learn some practices along the way. It's set up to help you answer some common questions that you might have while you navigate some potentially sticky situations."
+)
+
+st.write("You can use the sidebar to navigate to the tools you want to use, or scroll down to walkthrough some of the features.")
+
+st.markdown("---")
+st.write("Figuring out how to manage your money can be really hard. Coming soon, I'll be rolling out a budgeting tool to help you learn how to distribute your earnings and what your money can do for you.")
+c1, c2 = st.columns(2)
+
+with c1:
+    st.header("Budgeting Tools (Coming Soon)")
+
+with c2:
+    st.markdown("")
+
+st.markdown("---")
+st.write("Buying a home is a big decision, and it's important to know what you can afford. This tool will help you figure out how much you can afford to spend on a home, and how much you can expect to pay each month based on a few parameters.")
+
+c3, c4 = st.columns(2)
+
+with c3:
+    st.markdown("")
+
+with c4:
+    st.header("Home Buying Power Calculator")
+    
--- pages/Affordability_Calculator.py
+++ pages/Affordability_Calculator.py
@@ -54,55 +54,50 @@ st.write(
     "Use this tool to help forecast what market changes will do to your home buying power. But definitely read the disclaimer at the bottom."
 )
 
-st.write("Use these sliders to configure your parameters.")
+e1 = st.expander("Parameters", expanded=True)
 
-max_pay = st.slider(
-    "Max Monthly Payment",
-    min_value=0,
-    max_value=10_000,
-    step=100,
-    value=5_000,
-)
-max_down = st.slider(
-    "Down Payment", min_value=0, max_value=500000, step=1_000, value=100_000
-)
-max_rate = st.slider(
-    "Mortgage Interest Rate",
-    min_value=0.0,
-    max_value=10.0,
-    step=0.01,
-    value=today_30,
+with e1:
+    st.caption("Use these sliders to configure your parameters.")
+    max_pay = st.slider(
+        "Max Monthly Payment",
+        min_value=0,
+        max_value=10_000,
+        step=100,
+        value=5_000,
+    )
+    max_down = st.slider(
+        "Down Payment", min_value=0, max_value=500000, step=1_000, value=100_000
+    )
+    max_rate = st.slider(
+        "Mortgage Interest Rate",
+        min_value=0.0,
+        max_value=10.0,
+        step=0.01,
+        value=today_30,
+    )
+
+max_list = max_cost(max_pay, max_down, max_rate, 30)
+
+st.success(
+    f"Based on your parameters, your maximum purchase price is \${max_list:,.2f}."
 )
 
 # Create the main content
 st.markdown("---")
 
-tab1, tab2, tab3, tab4 = st.tabs(
-    ["Summary", "Price Affordability", "Rate Affordability", "Total Cost Comparison"]
-)
+tab1, tab2 = st.tabs(["Affordability Summary", "Total Cost Comparison"])
 
 with tab1:
-
-    max_list = max_cost(max_pay, max_down, max_rate, 30)
-
-    st.warning(
-        f"Your Selected Parameters are: \n\n Maximum Monthly Payment: \${max_pay} \n\n Down Payment: \${max_down}\n\n Estimated Interest Rate: {max_rate}\%"
-    )
-
-    st.success(
-        f"Based on your parameters, your maximum purchase price is \${max_list:,.2f}."
-    )
-with tab2:
     st.caption("This table shows the impact of price changes based on your parameters.")
     # price_table = price_afford_table(max_rate, max_down, 30, max_list)
     # st.table(price_table)
+    table_test = build_rate_price_matrix(today_30, max_list, max_down)
+    st.table(table_test)
 
-with tab3:
-    st.caption("This table shows the impact of rate changes based on your parameters.")
-    # rate_table = rate_afford_table(max_rate, max_down, 30, max_list)
-    # st.table(rate_table)
+    heat = heat_map()
+    st.altair_chart(heat, use_container_width=True)
 
-with tab4:
+with tab2:
     st.caption(
         "These graphs show the difference in total costs between different terms."
     )
@@ -110,12 +105,6 @@ with tab4:
     st.altair_chart(cost_chart, use_container_width=True)
 
 st.markdown("---")
-
-table_test = build_rate_price_matrix(today_30, max_list, max_down)
-st.table(table_test)
-
-heat = heat_map()
-st.altair_chart(heat, use_container_width=True)
 
 st.caption("Disclaimers")
 st.caption("Mortgage insurance is not included in these estimates.")
